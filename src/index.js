638aa1456c7acbfe039408d48d94f6c239a66e3e
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2019-12-02 13:36:12 +0800

    feat: add react base.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <h1>Hello World!</h1>
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
